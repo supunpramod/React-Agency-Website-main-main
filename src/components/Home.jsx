8520d83e26127260f755adf89013cf4a9e60a7bb
Supun Pramod <pramodsupun06@gmail.com>
2025-06-16 16:03:31 +0530

    feat: update Home component with Carousel implementation and improve layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,24 @@
 import React from "react";
 import { Carousel } from "flowbite-react";
 
-const Home =()=>{
-    return(
+const Home = () => {
+    return (
         <div className="bg-neutralSilver">
             <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen">
-                
-
-                <Carousel onSlideChange= {slideIdx => console.log("slide change")}>
-                <div cLassName= "flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                Slide 1
-                </div>
-                <div cLassName= "flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                Slide 2
-                </div>
-                <div cLassName= "flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-                Slide 3
-                </div>
-                
-               </Carousel>
-                
-                
-                 </div>
-            
+                <Carousel>
+                    <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+                        Slide 1
+                    </div>
+                    <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+                        Slide 2
+                    </div>
+                    <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+                        Slide 3
+                    </div>
+                </Carousel>
+            </div>
         </div>
-
     );
-
 };
+
 export default Home;
