fa973040167d978fc0e5b45a58fbc0c0be2ae0c1
supunpramod <pramodsupun06@gmail.com>
2025-07-09 10:06:31 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 
 const Blog = () => {
+    const blog=[
+        {id:1,
+         title:"Creating Streamlined Safeguarding Processes with OneRen",
+         image:"/src/assets/blog1.png",},
+         {id:2,
+         title:"What are your safeguarding responsibilities and how can you manage them?",
+         image:"/src/assets/blog2.png",},
+         {id:3,
+         title:"Revamping the Membership Model with Triathlon Australia",
+         image:"/src/assets/blog3.png",}
+    ]
     return (
         <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto my-12">
+            <div className="text-center md:w-1/2 mx-auto">
+                <h2 className='text 4xl text-neutralDGrey font-semibold mb-4 '>Caring is the new marketing</h2>
+                <p className=' text-sm text-neutralGrey mb-8 md:w-3/4 mx-auto'>The Nextcent blog is the best place to read about the latest membership insights, trends and more. See who's joining the community, read about how our community are increasing their membership income and lot's more.​</p>
+            </div>
+            {/* all blogs */}
             <div>
-                <h2 className='text 4xl text-neutralDGrey font-semibold mb-4 md:w-4/5'>The unseen of spending three years at Pixelgrade</h2>
-                <p className='md:w-3/4 text-sm text-neutralGrey mb-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+                {
+                    blog.map(blog=><div key={blog.id}>
+                        <img src={blog.image} alt=""/>
+                        <div>
+                            <h3>{blog.title}</h3>
+                            <p>Read more</p>
+                        </div>
+                    </div>)
+                }
             </div>
             </div>
     );
