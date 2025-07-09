dfb13b9f487db71f5b7a31c6e95c32c665290831
supunpramod <pramodsupun06@gmail.com>
2025-07-09 13:42:52 +0530

    feat: Add framer-motion for animations and update About and MyFooter components for improved layout and responsiveness

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -18,7 +18,7 @@ const MyFooter=()=>{
     return(
         <Footer container >
       <div className="w-full">
-        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+        <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1 px-4 lg:px-14 max-w-screen-2xl">
           <div className="space-y-4 mb-8">
             <a href="#" className="text-2xl font-semibold flex items-center space-x-3">
                        <img src={logo} alt="Logo" className="w-10 inline-block" />
@@ -29,7 +29,7 @@ const MyFooter=()=>{
                 <p>All rights reserved</p>
                 </div>        
           </div>
-          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6 ">
             <div>
               <FooterTitle title="about" />
               <FooterLinkGroup col>
@@ -54,8 +54,8 @@ const MyFooter=()=>{
           </div>
         </div>
         <FooterDivider />
-        <div className="w-full sm:flex sm:items-center sm:justify-between">
-          <FooterCopyright href="#" by="Flowbite™" year={2022} />
+        <div className="w-full sm:flex sm:items-center sm:justify-between px-4 lg:px-14 max-w-screen-2xl">
+          <FooterCopyright href="#" by="Flowbite™" year={2025} />
           <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
             <FooterIcon href="#" icon={BsFacebook} />
             <FooterIcon href="#" icon={BsInstagram} />
